37c0906dda6dca51d97da8662e0b70a5418bb6c3
william <191marquis@gmail.com>
2020-07-20 17:33:57 -0400

    [BUGFIX] Change List input to Tuple input

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,7 +7,7 @@ from typing import Tuple, List
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     zoomed_in: List = [
-        item for item in lst
+        item for item in list(lst)
         for i in range(factor)
     ]
     return zoomed_in
@@ -15,6 +15,6 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
 
 array = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(tuple(array), 3)
