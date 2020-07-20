a3c63a5ce8527ad351ddb629430c690ad8c43fad
william <191marquis@gmail.com>
2020-07-20 15:50:29 -0400

    [INIT] Do second try

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,8 +2,10 @@
 """ Python typing annotations """
 
 
-from typing import List, Tuple, Sequence, Iterable
+from typing import List, Tuple
+from typing import Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ 'Duck typed' """
     return [(i, len(i)) for i in lst]
