7bc1eb3d7d2963a0027fb5ee8eb62e57f817ed0e
william <191marquis@gmail.com>
2020-07-21 14:36:50 -0400

    [BUGFIX] Fix typo

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,5 +9,5 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """ Wait a random amount of delay and then return that number"""
     x = random() * max_delay
-    await syncio.sleep(x)
+    await asyncio.sleep(x)
     return x
