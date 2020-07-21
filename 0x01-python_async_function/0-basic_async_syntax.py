18e2dba741f506cc090b6005e83dd31fde9844dd
william <191marquis@gmail.com>
2020-07-21 14:38:43 -0400

    [BUGFIX] Fix function being called but not from module

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """ Wait a random amount of delay and then return that number"""
-    x = random() * max_delay
+    x = random.random() * max_delay
     await asyncio.sleep(x)
     return x
