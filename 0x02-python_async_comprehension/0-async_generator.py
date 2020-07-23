2aecac6b8a35df076d03cb2551d54296f1fa714e
william <191marquis@gmail.com>
2020-07-22 20:42:00 -0400

    [BUGFIX] Add bologna type declaration

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,12 +2,12 @@
 """ Testing async """
 
 
-from typing import List
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ Generator """
     for i in range(10):
         await asyncio.sleep(1)
