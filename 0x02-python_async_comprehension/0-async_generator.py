4de77d7268726bcafa01e4c3f4a4638a42f30a6f
william <191marquis@gmail.com>
2020-07-22 20:30:38 -0400

    [INIT] Create files

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Testing async """
+
+
+from typing import List
+import asyncio
+import random
+
+
+async def async_generator():
+    """ Generator """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() + 10
