2419a5cd2bcbb7002e5632601583ebb91c5083d5
william <191marquis@gmail.com>
2020-07-26 19:51:37 -0400

    [DEV] Improve code abstraction via an additional method

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -15,6 +15,33 @@ class LFUCache(BaseCaching):
         self.keys_in = {}
         self.keys_time = {}
 
+    def __discard(self):
+        """ discard an item from the queue """
+        lowk = None
+        lowv = None
+        lowt = None
+        for k, v in self.keys_in.items():
+            if lowk is None:
+                lowk = k
+            if lowv is None:
+                lowv = v
+            if lowt is None:
+                lowt = self.keys_time.get(k, time())
+            if v == lowv:
+                if self.keys_time.get(k, time()) < lowt:
+                    lowk = k
+                    lowv = v
+                    lowt = self.keys_time.get(k, time())
+            if v < lowv:
+                lowk = k
+                lowv = v
+                lowt = self.keys_time.get(k, time())
+
+        print("DISCARD: " + str(lowk))
+        del self.cache_data[lowk]
+        del self.keys_in[lowk]
+        del self.keys_time[lowk]
+
     def put(self, key, item):
         """ put an item in """
         if item is None or key is None:
@@ -22,30 +49,7 @@ class LFUCache(BaseCaching):
 
         curr = key in self.cache_data.keys()
         if len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS and not curr:
-            lowk = None
-            lowv = None
-            lowt = None
-            for k, v in self.keys_in.items():
-                if lowk is None:
-                    lowk = k
-                if lowv is None:
-                    lowv = v
-                if lowt is None:
-                    lowt = self.keys_time.get(k, time())
-                if v == lowv:
-                    if self.keys_time.get(k, time()) < lowt:
-                        lowk = k
-                        lowv = v
-                        lowt = self.keys_time.get(k, time())
-                if v < lowv:
-                    lowk = k
-                    lowv = v
-                    lowt = self.keys_time.get(k, time())
-
-            print("DISCARD: " + str(lowk))
-            del self.cache_data[lowk]
-            del self.keys_in[lowk]
-            del self.keys_time[lowk]
+            self.__discard()
 
         self.keys_in[key] = self.keys_in.get(key, 0) + 1
         self.keys_time[key] = time()
