bbd1cfbb2d90c69ae826112ed57fef1a6167a98f
william <191marquis@gmail.com>
2020-07-29 20:01:22 -0400

    [INIT] Do first try

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -2,6 +2,9 @@
 """ pagination """
 
 
-def index_range(page, page_size):
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ get the start and end range for two numbers """
     return ((page-1) * page_size, page * page_size)
