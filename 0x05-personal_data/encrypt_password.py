4989d3a6dbd1aaa962cd060a3bf765b323ab39e5
william <191marquis@gmail.com>
2020-08-03 22:01:44 -0400

    [INIT] Do task 5

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,6 +7,6 @@ Password Hash
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """hash a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
