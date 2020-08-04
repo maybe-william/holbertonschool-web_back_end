6146701f170a3f5efe9a213e74e79b0125f8f775
william <191marquis@gmail.com>
2020-08-03 22:06:11 -0400

    [DEV] Try task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -10,3 +10,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """hash a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if valid """
+    return bcrypt.hashpw(password.encode(),
+                         hashed_password) == hashed_password
