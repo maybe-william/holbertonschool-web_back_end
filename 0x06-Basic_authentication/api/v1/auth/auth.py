f71a2ce63cf05e8c1dd215cf6593b52eb8ae4131
william <191marquis@gmail.com>
2020-08-08 02:55:13 -0400

    [INIT] Do task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ check if auth required for path """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        nonslash = list([x[:-1] for x in excluded_paths if x[-1] == '/'])
+        if path in excluded_paths or path in nonslash:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ check auth info in request header """
