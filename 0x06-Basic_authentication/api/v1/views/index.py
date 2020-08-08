17092f318c63b3af81184ccfc900fed3e1eff13a
william <191marquis@gmail.com>
2020-08-08 02:41:11 -0400

    [INIT] Try task 2

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,13 @@ def unauthorized() -> str:
     """
     abort(401)
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - the forbidden of the API
+    """
+    abort(403)
+    return jsonify({"status": "OK"})
