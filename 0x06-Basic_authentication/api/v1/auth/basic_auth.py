84ffb382ed81358028ff66f62bf566d80a9f7160
william <191marquis@gmail.com>
2020-08-08 22:57:31 -0400

    [DEV] Accomodate colons

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,7 +47,12 @@ class BasicAuth(Auth):
             return none
         if ":" not in decoded_base64_authorization_header:
             return none
-        return tuple(decoded_base64_authorization_header.split(':'))
+        user_pass = decoded_base64_authorization_header.split(":")
+        print(user_pass)
+        if len(user_pass) > 2:
+            user_pass[1] = ":".join(user_pass[1:])
+            print(user_pass)
+        return (user_pass[0], user_pass[1])
 
     def user_object_from_credentials(
             self,
