d90ce458d128c4306e939908328a79656591443a
william <191marquis@gmail.com>
2020-08-12 02:02:47 -0400

    [DEV] Do task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -33,10 +33,13 @@ def before():
     if auth is not None:
         excl = ['/api/v1/status/',
                 '/api/v1/unauthorized',
-                '/api/v1/forbidden']
+                '/api/v1/forbidden',
+                '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excl):
+
             if auth.authorization_header(request) is None:
-                abort(401)
+                if auth.session_cookie(request) is None:
+                    abort(401)
             if auth.current_user(request) is None:
                 abort(403)
 
