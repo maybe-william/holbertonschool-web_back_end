f7a72a1c8a032ebdbf34e6bbdb53441b356e41af
william <191marquis@gmail.com>
2020-08-12 02:45:34 -0400

    [DEV] Do task 7

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Module of Users views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import environ
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_login() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      - list of all User objects JSON represented
+    """
+    email = request.form.get("email", None)
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    password = request.form.get("password", None)
+    if password is None:
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({"email": email})
+    if user is None or len(user) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    sess = auth.create_session(user.id)
+    ret = jsonify(user.to_json())
+    cookie_name = environ.get("SESSION_NAME", None)
+    ret.set_cookie(cookie_name, sess)
+    return ret
