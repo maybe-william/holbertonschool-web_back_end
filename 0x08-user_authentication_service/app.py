93ad093abc7485fc3413482676dd4023d40b3953
william <191marquis@gmail.com>
2020-08-19 03:59:07 -0400

    [DEV] Do task 7

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,10 +4,13 @@ Auth Flask App
 """
 
 
+import json
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
@@ -17,6 +20,19 @@ def root_route() -> str:
     """ get the root route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_route() -> str:
+    """ get the root route
+    """
+    body = request.form
+
+    try:
+        user = AUTH.register_user(body['email'], body['password'])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
