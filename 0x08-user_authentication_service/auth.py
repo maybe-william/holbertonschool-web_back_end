147d45b63fc58d7d38e3dcc3dc497f787659bb46
william <191marquis@gmail.com>
2020-08-19 03:14:21 -0400

    [INIT] Do task 5

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,6 +3,29 @@
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ init function """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ check if a user exists, otherwise register """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError
+        except NoResultFound:
+            pass
+        hsh = _hash_password(password)
+        u = self._db.add_user(email=email, hashed_password=hsh)
+        return u
 
 
 def _hash_password(password: str) -> str:
