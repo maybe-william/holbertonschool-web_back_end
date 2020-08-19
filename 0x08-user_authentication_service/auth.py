963e499ee7dfb3a139d51e6b545d4547099282a4
william <191marquis@gmail.com>
2020-08-19 18:59:42 -0400

    [DEV] Do task 13

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -56,6 +56,15 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """ destroy a session for a user """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> str:
     """ Hash a password """
