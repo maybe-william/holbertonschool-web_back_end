e1f5361c379be4cae896ae1030d44a7e92bbd43e
william <191marquis@gmail.com>
2020-08-28 17:47:01 -0400

    [DEV] Do task 1

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Flask App
+"""
+
+
+import json
+from flask import Flask, jsonify, abort, request, make_response, redirect
+from flask import render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def root_route() -> str:
+    """ get the root route
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.config.from_pyfile('babel.cfg')
+    babel = Babel(app)
+    app.run(host="0.0.0.0", port="5000")
