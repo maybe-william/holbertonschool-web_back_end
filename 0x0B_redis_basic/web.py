fd09cf73f5d07f871583e81d7340501d034602f8
william <191marquis@gmail.com>
2020-09-01 18:06:46 -0400

    [BUGFIX] Only increment count if not None

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -18,14 +18,15 @@ def my_cache(method: Callable) -> Callable:
     def wrap(*args, **kwargs):
         """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
         url = args[0]
-        red.incr("count:"+url, 1)
         text = red.get(url)
         if text is None:
             text = method(*args, **kwargs)
             if text is not None:
                 red.setex(url, 10, text)
+                red.incr("count:"+url, 1)
         else:
             text = text.decode()
+            red.incr("count:"+url, 1)
         return text
     return wrap
 
