0fc4a45dd829622a42c2ea6260904a1ee93c2a5c
william <191marquis@gmail.com>
2020-09-01 04:36:01 -0400

    [BUGFIX] Space out docstrings

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-The Redis Exercises module
+T h e  R e d i s  E x e r c i s e s  m o d u l e
 """
 
 import redis
@@ -11,19 +11,19 @@ from typing import Union
 
 class Cache:
     """
-    The redis cache class
+    T h e  r e d i s  c a c h e  c l a s s
     """
 
     def __init__(self):
         """
-        Init the redis cache
+        I n i t  t h e  r e d i s  c a c h e
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Return a key after storing data
+        R e t u r n  a  k e y  a f t e r  s t o r i n g  d a t a
         """
         key = str(uuid4())
         self._redis.set(key, data)
