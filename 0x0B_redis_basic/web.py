6ccd2004fe23ca3c4b08fa9ca09c98753e37da02
william <191marquis@gmail.com>
2020-09-01 18:21:26 -0400

    [TEST] Change kwargs to args in getting url

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -17,7 +17,7 @@ def my_cache(method: Callable) -> Callable:
     @wraps(method)
     def wrap(*args, **kwargs):
         """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
-        url = kwargs["url"]
+        url = args[0]
         text = red.get(url)
         if text is None:
             text = method(*args, **kwargs)
