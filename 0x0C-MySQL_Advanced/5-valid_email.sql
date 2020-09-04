0ca8280ce98a887944ce651273880fadfc990b04
william <191marquis@gmail.com>
2020-09-04 19:03:13 -0400

    [INIT] Do first try

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,3 +1,3 @@
 -- advanced SQL task
 -- trigger a valid email reset
-CREATE TRIGGER reset_valid_email AFTER UPDATE ON users FOR EACH ROW IF NEW.email <> OLD.email UPDATE users SET valid_email = 0 WHERE email = NEW.email;
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users FOR EACH ROW IF NEW.email <> OLD.email UPDATE users SET valid_email = 0 WHERE email = NEW.email;
