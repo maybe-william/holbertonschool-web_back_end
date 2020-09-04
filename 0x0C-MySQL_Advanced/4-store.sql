e60bd973f0a891b9ae7fb82562a8b70eae768f09
william <191marquis@gmail.com>
2020-09-04 18:53:12 -0400

    [BUGFIX] Fix mysql syntax

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,7 +1,3 @@
 -- advanced SQL task
 -- trigger a decrease in items
-CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
-    FOR EACH ROW
-    BEGIN
-        UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
-    END;
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders FOR EACH ROW UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
