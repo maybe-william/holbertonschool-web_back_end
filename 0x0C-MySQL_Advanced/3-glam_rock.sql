d4d8c62772e8a9f78ab2e89985fe32493f2ff028
william <191marquis@gmail.com>
2020-09-04 18:22:41 -0400

    [INIT] Do first try

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- advanced SQL task
 -- sort by longevity
-SELECT band_name, ifnull(split, 2020) - ifnull(formed, 0) AS longevity FROM metal_bands WHERE style LIKE '%glam rock%' ORDER BY longevity DESC;
+SELECT band_name, ifnull(split, 2020) - ifnull(formed, 0) AS longevity FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY longevity DESC;
