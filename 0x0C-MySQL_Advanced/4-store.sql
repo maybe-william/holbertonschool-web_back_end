84e6bd29e562135956f1e072ec1ae467eee3785e
william <191marquis@gmail.com>
2020-09-04 18:41:15 -0400

    [INIT] Do first try

---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,6 @@
+-- advanced SQL task
+-- trigger a decrease in items
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders FOR EACH ROW
+BEGIN
+    UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+END;
