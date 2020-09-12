7da1c3888a16a8c4d8eab0d310d7aa364e484ac0
william <191marquis@gmail.com>
2020-09-11 22:10:36 -0400

    [INIT] Do second try

--- 0x0E-ES6_basic/6-string-interpolation.js
+++ 0x0E-ES6_basic/6-string-interpolation.js
@@ -6,7 +6,8 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return `As of ${year}, it was the seventh-highest income county in the United States`
-        / `, with a per capita personal income of ${budget.income}. As of 2015, San Francisco`
-        / ` proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  const a = `As of ${year}, it was the seventh-highest income county in the United States`;
+  const b = `, with a per capita personal income of ${budget.income}. As of 2015, San Francisco`;
+  const c = ` proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  return a + b + c;
 }
