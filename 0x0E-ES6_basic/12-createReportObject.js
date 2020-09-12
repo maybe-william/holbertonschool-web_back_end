071952e9f209ee922e49a154cc1e34bc921f4a36
william <191marquis@gmail.com>
2020-09-11 23:58:46 -0400

    [INIT] Do seventh? try

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,15 +1,10 @@
 export default function createReportObject(employeesList) {
   const ret = {
-    allEmployees: [...employeesList],
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(EmployeesList) {
+        return [...employeesList].length / 2;
+    },
+
   };
-  let deptNum = 0;
-  for (const x in employeesList) {
-    if (x !== 'Something weird') {
-      ret[x] = employeesList[x];
-      deptNum += 1;
-    }
-  }
-  const getNum = () => deptNum;
-  ret.getNumberOfDepartments = getNum;
   return ret;
 }
