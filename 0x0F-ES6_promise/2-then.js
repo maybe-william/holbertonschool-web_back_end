d0fa8e459929b3c6558a34b6b99cd023dd41ca80
william <191marquis@gmail.com>
2020-09-16 16:15:12 -0400

    [INIT] Do fourth try

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,9 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
+  return promise.then((success) => {
     console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'success',
-    };
+    return success;
   }, () => new Error());
 }
