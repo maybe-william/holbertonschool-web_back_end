8e3754a7fdb478dfeb813cfc01399c375a54ccee
william <191marquis@gmail.com>
2020-09-16 17:15:29 -0400

    [INIT] Do second try

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const x = uploadPhoto();
   const y = createUser();
-  return Promise.all(x, y)
+  return Promise.all([x, y])
     .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`),
       () => console.log('Signup system offline'));
 }
