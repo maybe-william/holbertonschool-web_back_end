b8f6af5db224f5b20b01bfc09d52b07b73ea0804
william <191marquis@gmail.com>
2020-09-16 17:31:19 -0400

    [INIT] Do second try

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -4,5 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const x = uploadPhoto(fileName);
   const y = createUser(firstName, lastName);
-  return Promise.allSettled([y, x]);
+  return Promise.allSettled([y, x]).then((res) => {
+    for (const x of res) {
+      if (x.status === 'rejected') {
+        x.value = x.reason;
+        x.reason = undefined;
+      }
+    }
+  });
 }
