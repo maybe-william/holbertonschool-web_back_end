871966d5387234297caafa1f9afb856167ba6061
william <191marquis@gmail.com>
2020-09-21 16:47:09 -0400

    [INIT] Do second try

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -2,10 +2,9 @@ export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
-    this.toString = this._toString;
   }
 
-  _toString() {
+  toString() {
     return `[object] ${this._code}]`;
   }
 }
