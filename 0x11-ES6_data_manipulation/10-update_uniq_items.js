4b085a9a8cd912ab76313316166f31b61f4f46dc
william <191marquis@gmail.com>
2020-09-23 18:12:00 -0400

    [INIT] Do first try

---
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const x of map) {
+      if (x[1] === 1) {
+        map.set(x[0], 100);
+      }
+    }
+    return map;
+  }
+  throw Error('Cannot process');
+}
