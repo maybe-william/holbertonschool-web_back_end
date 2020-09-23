f6df9d9b2707990432b59bcdefef7e0163bd66a1
william <191marquis@gmail.com>
2020-09-23 15:22:51 -0400

    [INIT] Do second try

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getListStudentIdsSum(students) {
   if (!Array.isArray(students)) {
-    return [];
+    return 0;
   }
   let val = 0;
   val = students.reduce((prev, x) => prev + x.id);
