1ec267d2194944bd2885653b8855fee42a56713b
william <191marquis@gmail.com>
2020-09-23 17:25:43 -0400

    [INIT] Do third try

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  const x = new UInt8Array(length);
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  const x = new Int8Array(length);
   x.set(value, position);
   return x;
 }
