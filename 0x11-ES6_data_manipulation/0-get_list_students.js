f58f9390fd093222508fe18de13f69fdf073d5bf
william <191marquis@gmail.com>
2020-09-23 15:08:46 -0400

    [INIT] Do first try

---
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const g = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const j = { id: 2, firstName: 'James', location: 'Columbia' };
+  const s = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [g, j, s];
+}
