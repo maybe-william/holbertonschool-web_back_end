142fad711aa37d36961ebf3f80955f0c179e0fa7
william <191marquis@gmail.com>
2020-09-23 18:06:31 -0400

    [INIT] Do first try

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const x = new Map();
+  x.set('Apples', 10);
+  x.set('Tomatoes', 10);
+  x.set('Pasta', 1);
+  x.set('Rice', 1);
+  x.set('Banana', 5);
+  return x;
+}
